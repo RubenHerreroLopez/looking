d014b4b287d55e75a1963ba0fdde49529104ea3c
ruben <ru.he.lo.m@gmail.com>
2021-04-13 13:27:39 +0200

    main.xml main.java no-rotation

--- app/src/main/java/com/rubinsoft/looking/Usuario_Login.java
+++ app/src/main/java/com/rubinsoft/looking/Usuario_Login.java
@@ -2,13 +2,41 @@ package com.rubinsoft.looking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Usuario_Login extends AppCompatActivity {
 
+    EditText et_usuario, et_pass;
+    Button btn_login, btn_registro;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_usuario__login);
+
+        et_usuario = findViewById(R.id.UsuarioLogin_et_usuario);
+        et_pass = findViewById(R.id.UsuarioLogin_et_pass);
+        btn_login = findViewById(R.id.UsuarioLogin_btn_login);
+        btn_registro = findViewById(R.id.UsuarioLogin_btn_registro);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btn_registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Usuario_Login.this, Usuario_Registro.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/rubinsoft/looking/Usuario_Tablon.java
+++ app/src/main/java/com/rubinsoft/looking/Usuario_Tablon.java
@@ -2,12 +2,14 @@ package com.rubinsoft.looking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 public class Usuario_Tablon extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_usuario__tablon);
     }
